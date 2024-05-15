df7c5699302c94b83c8f28a2465c082a729cca32
ntnon <Anton.nydal@hotmail.com>
2024-05-15 20:44:39 +0200

    chore: Update +page.svelte component layout and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-    /** @type {import('./$types').PageData} */
-    import weatherTypes from '$lib/weatherTypes';
+	/** @type {import('./$types').PageData} */
+	import weatherTypes from '$lib/weatherTypes';
+
 	export let data;
-    const time = data.item.current.is_day === 1 ? "day" : "night";
-    const norskTid = time === "day" ? "Dag" : "Natt";
-    const weatherType = data.item.current.weather_code;
-    const image = weatherTypes[1][time].image;
-    const weatherDescription = weatherTypes[weatherType][time].description;
-    console.log(weatherTypes[weatherType]);
+	const time = data.item.current.is_day === 1 ? 'day' : 'night';
+
+	const weatherType = data.item.current.weather_code;
+	const image = weatherTypes[1][time].image;
 </script>
 
-<body>
-
-<main>
-	<div>{data.item.current.temperature_2m}{data.item.current_units.temperature_2m}</div>
-    <div>{time}</div>
-    <img src="{image}"/>
-    <div>{norskTid}</div>
-</main>
-
-</body>
+<div class="bg-gradient-to-b from-ks-light to-ks h-[100dvh] flex items-center">
+	<div class="max-w-max mx-auto my-auto">
+		<div class="weather-card bg-slate-50 rounded-xl p-12 space-y-3">
+			<h1 class="w-min mx-auto text-3xl">
+				{data.item.current.temperature_2m}{data.item.current_units.temperature_2m}
+			</h1>
+			<img class="w-[10rem]" src={image} />
+			<h2 class="w-min mx-auto text-3xl">{time === 'day' ? 'Dag' : 'Natt'}</h2>
+		</div>
+	</div>
+</div>
